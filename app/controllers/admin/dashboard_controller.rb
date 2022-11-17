719862f56947aea6ffae1b2b67fc0bb0a5f52650
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-11-17 18:16:30 +0000

    Added to admin dashboard page so that it shows basic stats of products and categories in our database. Done using active record queries from within admin dashboard controller and passing instance variables containing query results to show page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,9 @@ class Admin::DashboardController < ApplicationController
                                password: ENV['HTTP_BASIC_PASSWORD']
 
   def show
+    @products = Product.all
+    @count_of_products = @products.count
+    @categories = Category.all
+    @count_of_categories = @categories.count
   end
 end
