d481d8ec8367973ac1144760eb803ddd9b99e78a
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-11-18 00:40:26 +0000

    Added personal notes to file

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -9,7 +9,7 @@ class Admin::CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params)
-
+    #Here with save, we save our @category to the database and then redirect back to categories page.
     if @category.save
       redirect_to [:admin, :categories], notice: 'Category created!'
     else
@@ -18,7 +18,7 @@ class Admin::CategoriesController < ApplicationController
   end
 
   private
-
+  #Cateogry params here takes the information that is in the form and stores it. 
   def category_params
     params.require(:category).permit(
       :name
