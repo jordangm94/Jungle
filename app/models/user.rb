a667c121fd677de43cffb7026a662d67a32099ca
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-11-18 01:20:00 +0000

    Added new users model, corresponding users controller, updated schema to contain users table, and added users route to application

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+# This model was made using the rails generate model user name email password_digest command
+# On that command, we created this model, along with it's corresponding table that has all the columns
+
+class User < ApplicationRecord
+end
