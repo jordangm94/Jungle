4a6d4e5411d33d2be4e4ace56b1945af8eb2f98e
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-11-21 22:15:05 +0000

    Created product_spec for product model and added initial it statements for incoming tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+# This model r spec file was instantly generated with the command rails generate rspec:model product
+
+RSpec.describe Product, type: :model do
+  describe "Validations" do
+
+    it "are valid with all attributes"
+    end
+
+    it "are not valid without a title"
+    end
+
+    it "are not valid without a quantity"
+    end
+
+    it "are not valid without a category"
+    end
+
+  end
+end
