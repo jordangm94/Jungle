85482057b418f2b1d6a26f292ad0c105e18f5faf
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-11-21 23:22:56 +0000

    Readjusted syntax of @product creation, created @product and @category baseline prior to tests. Adjusted @product accordingly for each test. Implemented 4 more tests each with their own corresponding error message

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,21 +1,44 @@
 require 'rails_helper'
 
 # This model r spec file was instantly generated with the command rails generate rspec:model product
+# The be_valid method tests if the given model instance's valid? method is true
 
 RSpec.describe Product, type: :model do
+  
   describe "Validations" do
-
-    it "are valid with all attributes"
+    #This before establishes a base @category and @product for our upcoming tests, we will adjust values accordingly.
+    before(:each) do
+      @category= Category.new(name: "Anything")
+      @product = Product.new(name: "Anything", price_cents: 2, quantity: 3, category: @category)
     end
 
-    it "are not valid without a title"
+    it "are valid with all attributes" do
+      #Here we ensure that we can successfully save @product with all attributes expect it to be valid
+      @product.save
+      expect(@product).to be_valid
     end
+    
 
-    it "are not valid without a quantity"
+    it "are not valid without a title" do
+      @product.name = nil
+      expect(@product).not_to be_valid
+      @product.errors.full_messages
     end
+    
 
-    it "are not valid without a category"
+    it "are not valid without a quantity" do
+      @product.quantity = nil
+      expect(@product).not_to be_valid
+      @product.errors.full_messages
     end
+    
+
+    it "are not valid without a category" do
+      @product.category = nil
+      expect(@product).not_to be_valid
+      @product.errors.full_messages
+    end
+    
 
   end
 end
