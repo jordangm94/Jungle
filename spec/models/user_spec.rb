288b844545385fb50a421ece1e389aaaaf4b7c94
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-11-22 00:00:34 +0000

    Added tests that ensured that user must provide a name, email and password, and lack of name or email  should result in failed validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,25 @@ RSpec.describe User, type: :model do
   describe "Validations" do
 
     before(:each) do
-      @user= User.new(name: "Jordan", email: "jordan.test@gmail.com")
+      @user= User.new(name: "Jordan", email: "jordan.test@gmail.com", password_digest: "testing")
+    end
+
+    it "are valid with all attributes" do
+      #Here we ensure that we can successfully save @product with all attributes expect it to be valid
+      @user.save
+      expect(@user).to be_valid
+    end
+
+    it "are not valid without a name" do
+      @user.name = nil
+      @user.errors.full_messages
+      expect(@user).not_to be_valid
+    end
+
+    it "are not valid without an email" do
+      @user.email = nil
+      @user.errors.full_messages
+      expect(@user).not_to be_valid
     end
 
 
