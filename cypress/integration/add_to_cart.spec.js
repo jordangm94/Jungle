9715b8a53917807a7988224e2b1bbe802de26dd5
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-11-23 01:50:40 +0000

    Modified cypress add to cart feature spec in order to ensure that cart does not have 1 item at beginning of test, and after clicking add durinig test, it then has 1 item in cart at end of test

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -2,9 +2,11 @@ describe('Clicking add to cart on a product on home page', () => {
   
   it("Clicking on add to cart on home page should increase cart from 0 to 1", () => {
     cy.visit('/')
+    cy.contains('My Cart (1)').should('not.exist') //Ensure cart does not have 1 item in it at beginning of test just to be safe
     cy.contains(".btn", "Add")
     .first()
     .click({force: true})
+    cy.contains('My Cart (1)').should('exist') //Due to the actions of our test, we now validate that cart has 1 item from us clicking add
   });
 
 })
